271294730df3805bc5efe0ce70d6d4222aa41ab1
Juliette Mahé <mahe.juliette@outlook.com>
2022-05-24 18:16:08 +0200

    removed unused files and functions

--- app.js
+++ app.js
@@ -1,35 +1,15 @@
-/*const connection = require("./db-config");*/
 const express = require("express"); // ok importe le paquet express
 const app = express(); // créé l'app express 
-/*const spawn = require(("child_process").spawn);*/
 const cors = require("cors");
-/*const bodyParser =require('body-parser');*/
-
 const router = require("./routes/index.routes");
 require('dotenv').config();
-console.log(process.env.PORT);
+
 const port = process.env.PORT || 6300;
 
-/*const corsOptions = {
-  origin: process.env.CLIENT_URL,
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};*/
-
-/*connection.connect((err) => {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-  } else {
-    console.log("connected as id " + connection.threadId);
-  }
-});*/
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-/*app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));*/
 
-/*app.use(cors(corsOptions));*/
 app.use(cors());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -45,18 +25,6 @@ app.use("/api", router);
 app.get("/", async (req, res) => {
   res.send("Welcome");
 });
-
-// Ecoute la méthode POST vers OpenAI API
-/*app.post("/get-message", (req, res) => {
-
-  console.log(req.body.message)
-  const pythonProcess = spawn("python", ["./api.py", req.body.message ]);
-
-  pythonProcess.stdout.on("data", function (data) {
-     res.setHeader("Content-Type", "application/json");
-     res.end(data.toString())
-  });
-})*/
 
 // Démarrer le serveur et écouter un port donné
 app.listen(port, () => {
