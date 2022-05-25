d15eb8394d246f37f21fb8ebd177ba771f18b761
MAHE JULIETTE <mahejuliette@outlook.com>
2022-05-25 16:07:08 +0200

    some functions for algo

--- utils/scoring.js
+++ utils/scoring.js
@@ -26,6 +26,85 @@ const scoringBeta = (phrase) => {
     return finalScore;
 }
 
+// Ici monte les différentes parties du scoring avant de les assembler
+
+let str = "Join 😀";
+let tempScore=0;
+
+// 1.Length 20%
+
+function stringLength(str) {
+    if ((str.length > 30) && (str.length < 52)){
+        tempScore += 10
+    } else {
+        tempScore += 0
+    }
+    return tempScore;
+}
+console.log(stringLength(str));
+
+function wordCount(str) {
+    return str.split(" ").length;
+    if ((str.split(" ").length > 6) && (str.split(" ").length > 6)){
+        tempScore += 10
+    } else {
+        tempScore +=0
+    }
+    return tempScore;
+}
+console.log(wordCount(str));
+
+//  2.Syntax 35%
+
+function allCaps(str) {
+    if (str.toUpperCase() === str){
+        true
+        tempScore -= 10
+    } else {
+        false
+        tempScore += 10
+    }
+    return tempScore;
+}
+console.log(allCaps(str));
+
+function allLowerC(str) {
+    if (str.allLowerC() === str){
+        true
+        tempScore -= 10
+    } else {
+        false
+        tempScore += 10
+    }
+    return tempScore;
+}
+console.log(allLowerC(str));
+
+function includeExclam(str) {
+    if (str.includes("!")){
+        tempScore -= 10  // BAD
+    } else {
+        tempScore =+ 0   // No impact
+    }
+    return tempScore;
+}
+console.log(includeExclam(str));
+
+function countOccurenceQ(str, word) {
+    if ((str.split("?").length-1) === 1) {
+        tempScore +=10
+    } else if ((str.split("?").length-1) === 1) {
+        tempScore -= 10
+    } else {
+        tempScore =+ 0
+    }
+    return tempScore;
+}
+console.log(countOccurenceQ(str, "?"));
+
+
+
+
 module.exports = {
     scoringSentences
 }
