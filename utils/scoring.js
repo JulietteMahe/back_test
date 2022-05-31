d0d85d376e8da8423b721f7db91b86276ee75437
MAHE JULIETTE <mahejuliette@outlook.com>
2022-05-31 08:37:17 +0200

    algo failing for values under 1

--- utils/scoring.js
+++ utils/scoring.js
@@ -1,3 +1,5 @@
+const { emojiPattern } = require("./emojiPattern");
+
 const scoringSentences = (resultArray, sentence) => {
     const finalArray = [];
     const tempScore = scoringBeta(sentence)
@@ -26,85 +28,176 @@ const scoringBeta = (phrase) => {
     return finalScore;
 }
 
-// Ici monte les différentes parties du scoring avant de les assembler
+// Ici je monte les différentes parties du scoring avant de les assembler
 
-let str = "Join us for a fun day in Europa park 😀 !";
+let str = "Jjjjjj tttttt ffffff ffffff aaaaaa wwwwwww 😇 !";
+
 let tempScore = 0;
+let tempScoreLength = 0;
+let tempScoreSyntax = 0;
+let tempScoreSemantic = 0;
 
-// 1.Length 20%
+
+//////////////////////  1.Length 20% score from 0,01 to 0,99
+
+//  String length -> functionning, lack weight
 
 function stringLength(str) {
-    if ((str.length > 30) && (str.length < 52)){
-        tempScore += 10
+    let lengthScore = 0;
+    // test includes emoji because emojies modify total case (between 2 et 4 case for 1 emoji)
+    
+    if (emojiPattern.test(str) == true) {
+        if ((str.length > 32) && (str.length < 54)){
+            lengthScore += 1
+        } else {
+            lengthScore += 0,5
+        }
     } else {
-        tempScore += 0
+        if ((str.length > 30) && (str.length < 52)){
+            lengthScore += 1
+        } else {
+            lengthScore += 0,5
+        }
     }
-    return tempScore;
+    return lengthScore;
 }
-console.log(stringLength(str));
+//console.log(stringLength(str));
+tempScoreLength += stringLength(str)
+console.log(tempScoreLength);
 
-function wordCount(str) {
-    if ((str.split(" ").length > 6) && (str.split(" ").length > 6)){
-        tempScore += 10
+
+//  Word count -> fonctionnelle , manque la pondération
+
+function countWords(str) {
+    let wordsCount = 0;
+    // test include emoji car modifie le word count total (entre 1 et 4 word pour 1 emoji)
+
+    if (emojiPattern.test(str) == true) {
+        if ((str.split(" ").length > 7) && (str.split(" ").length < 12) ){
+            wordsCount += 1
+        } else {
+            wordsCount += 0,5
+        }
     } else {
-        tempScore += 0
+        if ((str.split(" ").length > 6) && (str.split(" ").length < 10 )){
+            wordsCount += 1
+        } else {
+            wordsCount += 0,5
+        }
     }
-    return tempScore;
+    return wordsCount;
 }
-console.log(wordCount(str));
+console.log(countWords(str))
+console.log(tempScoreLength);
+tempScoreLength += countWords(str)
+console.log(tempScoreLength);
+tempScoreLength / 2
+console.log("length score is");
+console.log(tempScoreLength / 2);
 
-//  2.Syntax 35%
+
+////////////////////// 2.Syntax 35%
+
+
+//  All caps -> fonctionnelle , manque la pondération
 
 function allCaps(str) {
-    if (str.toUpperCase() === str){
-        true
-        tempScore -= 10
+    let capsScore = 0;
+    let newStr = str.toUpperCase();
+    if (newStr === str){
+        capsScore += 0,25
     } else {
-        false
-        tempScore += 10
+        capsScore += 0,75
     }
-    return tempScore;
+    return capsScore;
 }
-console.log(allCaps(str));
+//console.log(allCaps(str));
+tempScoreSyntax += allCaps(str)
+//console.log(tempScore);
+
+
+
+//  All lower case -> fonctionnelle , manque la pondération
 
 function allLowerC(str) {
-    if (str.allLowerC() === str){
-        true
-        tempScore -= 10
+    let allLowerScore = 0;
+    let newStr = str.toLowerCase();
+    if (newStr === str){
+        allLowerScore += 0,25
     } else {
-        false
-        tempScore += 10
+        allLowerScore += 0,75
     }
-    return tempScore;
+       
+    return allLowerScore;
 }
-console.log(allLowerC(str));
+//console.log(allLowerC(str));
+tempScoreSyntax += allLowerC(str)
+//console.log(tempScore);
+
+
+//  Nombre de ! -> fonctionnelle , manque la pondération
 
 function includeExclam(str) {
+    let exclamScore = 0;
     if (str.includes("!")){
-        tempScore -= 10  // BAD
+        exclamScore += 0,1  // BAD
     } else {
-        tempScore =+ 0   // No impact
+        exclamScore += 0,50   // No impact
     }
-    return tempScore;
+    return exclamScore;
 }
-console.log(includeExclam(str));
+//console.log(includeExclam(str));
+tempScoreSyntax += includeExclam(str)
+//console.log(tempScore);
+
+
+
+//  Nombre de ? -> fonctionnelle , manque la pondération
 
 function countOccurenceQ(str, word) {
+    let qScore = 0;
     if ((str.split("?").length-1) === 1) {
-        tempScore +=10
-    } else if ((str.split("?").length-1) === 1) {
-        tempScore -= 10
+        qScore += 0,75
+    } else if ((str.split("?").length-1) > 1) {
+        qScore += 0,25
     } else {
-        tempScore =+ 0
+        qScore += 0,5
     }
-    return tempScore;
+    return qScore;
 }
-console.log(countOccurenceQ(str, "?"));
+//console.log(countOccurenceQ(str));
+tempScoreSyntax += countOccurenceQ(str)
+//console.log(tempScore);
 
-/*function emojiOccurence(str)*/
 
 
+//  Nombre d'emoji -> fonctionnelle , manque la pondération
 
+function emojiOccurence(str) {
+    let emojiScore = 0;
+    emojiCount = ((str || '').match(emojiPattern) || []).length;
+    if (emojiCount === 0){
+        emojiScore += 0,5
+    } else if (emojiCount === 1){
+        emojiScore += 0,75
+    } else {
+        emojiScore += 0,25
+    }
+    return emojiScore;
+}
+//console.log(emojiOccurence(str));
+tempScoreSyntax = (tempScoreSyntax + emojiOccurence(str)) / 5
+//console.log("syntax score is");
+//console.log(tempScoreSyntax);
+
+
+/// TOTAL SCORE must be between 1 and 100
+/*function totalProp (str) {
+    return totalScore = (((tempScoreLength) * 0,40) + ((tempScoreSyntax) *0,60));
+}*/
+
+/*console.log("total score is");
+console.log(totalProp(str));*/
 
 module.exports = {
     scoringSentences
