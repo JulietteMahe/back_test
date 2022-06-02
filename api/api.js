e310fb4fea7d711b152dea3389c58b62c2b271e5
MAHE JULIETTE <mahejuliette@outlook.com>
2022-06-02 12:32:34 +0200

    changes on api

--- api/api.js
+++ api/api.js
@@ -30,7 +30,7 @@ const completionOpenAI = async (sentence) => {
 function generatePrompt(sentence) {
   const capitalizedSentence =
     sentence[0].toUpperCase() + sentence.slice(1).toLowerCase();
-  return `Generate three very different versions of our subject.
+  return `Rephrase the following subject line in five different ways (informal, formal, neutral, slang, then with an emoji)
 
 Sentence: Come discover our brand new products!
 Names: Click here to come discover our new stuff !!, Lots of new clothes for spring 🌻, Our latest products arrived
