5a2c4643016f2c45826bef521ac61db05041c8e3
MAHE JULIETTE <mahejuliette@outlook.com>
2022-06-02 14:22:37 +0200

    api call functionning with full generatePrompt option

--- api/api.js
+++ api/api.js
@@ -21,7 +21,7 @@ const fetchAlternativesFromOpenAI = async (sentence) => {
     temperature: 1,
     max_tokens: 182,
     top_p: 1,
-    frequency_penalty: 0,
+    frequency_penalty: 0.1,
     presence_penalty: 0,
   });
   return completion.data.choices[0].text;
@@ -30,14 +30,30 @@ const fetchAlternativesFromOpenAI = async (sentence) => {
 function generatePrompt(sentence) {
   const capitalizedSentence =
     sentence[0].toUpperCase() + sentence.slice(1).toLowerCase();
-  return `Rephrase the following email subject line in five alternatives ways (informal, formal, neutral, slang, then with an emoji): "${capitalizedSentence}"
-`;
+  return `Rephrase the following email subject line in five alternatives ways (informal, formal, neutral, slang, then with an emoji).
+  
+  Sentence: Come discover our brand new products!
+  Names: Click here to discover what's new !, Exclusive new items for our most valuable customer, Our latest products arrived, Check out our sick stuff for a hot girl summer !!, Lots of new clothes for spring 🌻
+  Sentence: It's back in stock! 
+  Names: Buy your favourite now before it's too late, Your favourites are back in stock, Your favorite product is back, I want i I fav it I buy it !!! , New arrivals solding out fast 🤯 
+  
+  Sentence: ${capitalizedSentence}
+  Names:`;
 }
 
 module.exports = {
   validate,
   fetchAlternativesFromOpenAI,
 }
+
+/*Generate three very different versions of our subject.
+
+Sentence: Come discover our brand new products!
+Names: Click here to come discover our new stuff !!, Lots of new clothes for spring 🌻, Our latest products arrived
+Sentence: It's back in stock! 
+Names: Your favourites are back in stock, Your favorite product is back, Buy your favourite now before it's too late
+Sentence: ${capitalizedSentence}
+Names:*/
 
 /*Sentence: Come discover our brand new products!
 Names: Click here to come discover our new stuff !!, Lots of new clothes for spring 🌻, Our latest products arrived
