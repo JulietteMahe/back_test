a1997fa8ea8d2f5eeb43b8b65b58848f307333cb
MAHE JULIETTE <mahejuliette@outlook.com>
2022-06-03 10:53:57 +0200

    add frequency penalty

--- api/api.js
+++ api/api.js
@@ -21,7 +21,7 @@ const fetchAlternativesFromOpenAI = async (sentence) => {
     temperature: 1,
     max_tokens: 182,
     top_p: 1,
-    frequency_penalty: 0,
+    frequency_penalty: 0.3,
     presence_penalty: 0,
   });
   return completion.data.choices[0].text;
